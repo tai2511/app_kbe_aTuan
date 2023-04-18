2862127e21979662ec0d8688220f76fd969c991e
tai2511 <nguyentai2511myv@gmail.com>
2023-04-18 22:23:11 +0700

    update

--- resources/views/components/vertical-layout1.blade.php
+++ resources/views/components/vertical-layout1.blade.php
@@ -11,7 +11,6 @@
                 <div class="p-content">
                     <p>Madre Alemana, padre italiano, título de bachillerato francés, licenciado en derecho y abogado en España. Experiencia plurianual en los contenciosos sobre contratos y sociedades. Desde febrero 2017 trabaja únicamente en el ámbito del derecho inmobiliario y solo en Mallorca, sobre todo en los términos municipales de Calviá y Andratx. Centenares de compras y ventas cerradas con suceso para clientes internacionales. Independiente, competente, fácil de contactar, atento al contexto y consciente de la rapidez necesaria en el ámbito de los negocios. <br /> ue lorem tortor fringilla sed,vestibulum id, eleifend justo  bib <br />-<br /> Abogado del Ilustre Colegio de Abogados de las Islas Baleares (número de colegiado 6476). <br />Oficina con aparcamiento privado en Avenida Jaume I 90, 07180 Santa Ponsa. Se recomienda acordar una cita. </p>
                 </div>
-                <button class="btn view-more">view more</button>
             </div>
         @else
             <div class="feature-content">
@@ -20,7 +19,6 @@
                 <div class="p-content">
                     {!! $data->content !!}
                 </div>
-                <button class="btn view-more">view more</button>
             </div>
         @endif
     </div>
